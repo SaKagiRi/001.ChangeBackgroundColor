ee41d17d0edd7f8b0e3824eecae3838bb2557ec2
SaKagiRi <kasichonooo@gmail.com>
2024-06-27 11:31:22 +0700

    Initial commit

---
+++ command.js
@@ -0,0 +1,12 @@
+const display =  document.getElementById('bkg');
+//const colorIn = document.getElementById('colorIn');
+//display.style.backgroundColor = color;
+
+const text = document.getElementById('textIn');
+const btn = document.getElementById('btn');
+
+
+
+btn.onclick = function() {
+    alert(text.value);
+}
