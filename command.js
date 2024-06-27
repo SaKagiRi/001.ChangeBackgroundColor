555175ce02ae8c655073b001fbe397087a175423
SaKagiRi <kasichonooo@gmail.com>
2024-06-27 23:07:05 +0700

    finish frist mini project
    yeyy ในที่สุดก็เสร็จสำหรับโปรแจ็คแรก สิ่งที่รู้สึกว่ายากก็คือJs หา .style ไม่เจอ นึกว่าเป็นerror หาวิธีแก้กันนานมากๆ อีกอย่างเลยก็คือวิธีการเก็บค่าจากinput เพราะมันรับมาเป็นelement เลยต้องนั่งงมกันนานมากถ้ามีคนมาบอกว่าต้องใช้ .value ก็คงง่านไปนานละ เอาหล่ะ ลุยโปรเจ็คต่อไปกัน!!

--- command.js
+++ command.js
@@ -1,12 +1,10 @@
 const display =  document.getElementById('bkg');
-//const colorIn = document.getElementById('colorIn');
-//display.style.backgroundColor = color;
-
-const text = document.getElementById('textIn');
+const colorIn = document.getElementById('colorIn');
 const btn = document.getElementById('btn');
-
-
-
+const background = document.querySelector('body');
+const text = document.getElementById('text');
 btn.onclick = function() {
-    document.write(text.value);
+    text.style.backgroundColor = colorIn.value;
+    text.style.border = 0;
+    background.style.backgroundColor =colorIn.value;
 }
